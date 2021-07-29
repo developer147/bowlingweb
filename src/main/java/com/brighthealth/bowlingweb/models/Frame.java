8dbf76e85b9fe67e060cb16a3fd38cfacfe95d15
Jay Harkar <jay.harkar@cerner.com>
2021-07-28 22:20:01 -0500

    Fixed conflicts

--- src/main/java/com/brighthealth/bowlingweb/models/Frame.java
+++ src/main/java/com/brighthealth/bowlingweb/models/Frame.java
@@ -40,16 +40,14 @@ public class Frame {
 		this.frameNumber  = frameNumber;
 	}
 	
+	
 	public void setScore(int score) {
 		this.score = score;
 	}
 	
 	public Roll getRoll3() {
-	}
-	
-	public void setScore(int score) {
-		this.score = score;
-	}
+		return roll3;
+	}	
 	
 	// Roll3 is special. Only applicable in 10th frame when second roll
 	// is either a strike or a spare.
@@ -70,21 +68,6 @@ public class Frame {
 	public int getScore() {
 		return score;
 	}	
-	
-	// Roll3 is special. Only applicable in 10th frame when second roll
-	// is either a strike or a spare.
-	public void setRoll3(Roll roll) {
-		if (frameNumber != FrameNumber.TEN) {
-			throw new RuntimeException("Cannot allow roll 3 except on 10th frame. You're on " + frameNumber.getValue());
-		}
-		//assert frameNumber == FrameNumber.TEN;
-		if (!(roll1 == Roll.STRIKE || (roll2  == Roll.STRIKE || roll2 == Roll.SPARE))) {
-			throw new RuntimeException("Can only allow roll 3 on 10th frame when roll 1 is a strike OR "
-					+ "roll2 is a strike/spare. You're on frame: " + frameNumber.getValue() + ", roll 1:" + roll1.getScore()
-					+ ", and roll2:" + roll2.getScore());
-		}
-		this.roll3 = roll;
-	}
 	
 	@Override
 	public String toString() {
